bb8227ef8bb0c6764bc0e41c5a604939f6e3c3fb
Winston <faluovo@gmail.com>
2019-09-06 09:21:20 -0700

    Add files via upload

---
+++ Demo.scala
@@ -0,0 +1,15 @@
+object Demo
+   {
+   def main(args: Array[String]): Unit = 
+      {
+      var myVar :Int = 10;
+      val myVal :String = "Hello Scala with datatype declaration.";
+      var myVar1 = 20;
+      val myVal1 = "Hello Scala new without datatype declaration.";
+      
+      println(myVar);
+      println(myVal);
+      println(myVar1); 
+      println(myVal1);
+      }
+   }
